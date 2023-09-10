c9f81fc8ab039ad3eb3e107a5b05dbb57842798f
limFakson <fakeyejoshua2005@gmail.com>
2023-09-10 04:46:05 +0100

    lag

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 document.querySelectorAll('.pro-img img').forEach(image => {
     image.onclick = () => {
         document.querySelector('.popup-img').style.display = 'block';
+        document.querySelector('.fa').style.display = 'block';
+        document.body.style.overflow = 'hidden';
         document.querySelector('.popup-img img').src = image.getAttribute('src');
     }
 });
 
-document.querySelector('.popup-img span').onclick = () => {
+// document.querySelector('.popup-img span').onclick = () => {
+//     document.querySelector('.popup-img').style.display = 'none';
+// }
+
+function closeImg() {
     document.querySelector('.popup-img').style.display = 'none';
+    document.body.style.overflow = 'auto';
 }
 console.log('working')
